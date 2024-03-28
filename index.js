ffd10e42accaf5a2e544b905945b3cea3fe51520
Alex Wang <iamalex11303@gmail.com>
2024-03-27 22:59:57 -0400

    added unique hashing and redirect

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
 require('dotenv').config();
+const dns = require('dns');
+const { URL } = require('url');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+// Stores URLS
+const short_urls = new Map();
+
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// Generate a unique id
+function genShortURL() {
+  var id = Math.random().toString(36).substring(2, 8);
+  while (short_urls.has(id)) {
+    id = Math.random().toString(36).substring(2, 8);
+  }
+  return id;
+}
+
+app.post("/api/shorturl", (req, res) => {
+  // Parse the URL and hostname
+  const link = req.body.url;
+  const purl = new URL(link);
+  const host = purl.hostname;
+
+  // Validate URL
+  dns.lookup(host, (err, address, family) => {
+    if (err) res.json({ "error": "invalid url" });
+
+    // Create id and save
+    const id = genShortURL();
+    short_urls.set(id, link);
+    res.json({
+      "original_url": link,
+      "short_url": id // value is unique for each url
+    });
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:url", (req, res) => {
+  res.redirect(short_urls.get(req.params.url));
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
